dc9406cbaae52038d5184324195f801ebdba4c34
herve <mugunga5@gmail.com>
2021-02-17 00:52:37 +0200

    Add section second

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,64 +1,61 @@
-let content = document.getElementById('content');
+const content = document.getElementById('content');
 
 const headerLayout = () => {
-    const head = () => {
-        const navClasses = 'navbar navbar-expand-lg navbar-light bg-light'.split(' ',)
-        const navCollapseClasses = 'collapse navbar-collapse'.split(' ',);
-        const ulClasses = 'navbar-nav ml-auto'.split(' ',);
-        
-       
+  const head = () => {
+    const navClasses = 'navbar navbar-expand-lg navbar-light bg-light'.split(' ');
+    const navCollapseClasses = 'collapse navbar-collapse'.split(' ');
+    const ulClasses = 'navbar-nav ml-auto'.split(' ');
 
-        const header = document.createElement('header');
-        const nav = document.createElement('nav');
-        const navContainer = document.createElement('div');
-        const navCollapse = document.createElement('div');
-        const ul = document.createElement('ul');
+    const header = document.createElement('header');
+    const nav = document.createElement('nav');
+    const navContainer = document.createElement('div');
+    const navCollapse = document.createElement('div');
+    const ul = document.createElement('ul');
 
-        const liHome = document.createElement('li');
-        const liMenu = document.createElement('li');
-        const liContact = document.createElement('li');
+    const liHome = document.createElement('li');
+    const liMenu = document.createElement('li');
+    const liContact = document.createElement('li');
 
-        const anchorHome = document.createElement('a');
-        const anchorMenu = document.createElement('a');
-        const anchorContact = document.createElement('a');
-        
-        header.classList.add = ('top-navbar');
-        nav.classList.add(...navClasses);
-        navContainer.classList.add('container');
-        navCollapse.classList.add(...navCollapseClasses);
-        navContainer.appendChild(navCollapse);
-        ul.classList.add(...ulClasses);
-        liHome.classList.add('nav-item');
-        liMenu.classList.add('nav-item');
-        liContact.classList.add('nav-item');
+    const anchorHome = document.createElement('a');
+    const anchorMenu = document.createElement('a');
+    const anchorContact = document.createElement('a');
 
-        anchorHome.classList.add('nav-link')
-        anchorMenu.classList.add('nav-link');
-        anchorContact.classList.add('nav-link')
+    header.classList.add = 'top-navbar';
+    nav.classList.add(...navClasses);
+    navContainer.classList.add('container');
+    navCollapse.classList.add(...navCollapseClasses);
+    navContainer.appendChild(navCollapse);
+    ul.classList.add(...ulClasses);
+    liHome.classList.add('nav-item');
+    liMenu.classList.add('nav-item');
+    liContact.classList.add('nav-item');
 
-        anchorHome.textContent = 'Home'
-        anchorMenu.textContent = 'Menu';
-        anchorContact.textContent = 'Contact'
+    anchorHome.classList.add('nav-link');
+    anchorMenu.classList.add('nav-link');
+    anchorContact.classList.add('nav-link');
 
-        anchorHome.setAttribute('id','home');
+    anchorHome.textContent = 'Home';
+    anchorMenu.textContent = 'Menu';
+    anchorContact.textContent = 'Contact';
 
-        liHome.appendChild(anchorHome);
-        liMenu.appendChild(anchorMenu)
-        liContact.appendChild(anchorContact)
+    anchorHome.setAttribute('id', 'home');
 
-        ul.appendChild(liHome);
-        ul.appendChild(liMenu);
-        ul.appendChild(liContact);
+    liHome.appendChild(anchorHome);
+    liMenu.appendChild(anchorMenu);
+    liContact.appendChild(anchorContact);
 
-        navCollapse.appendChild(ul);
-        nav.appendChild(navContainer);
-        header.appendChild(nav);
+    ul.appendChild(liHome);
+    ul.appendChild(liMenu);
+    ul.appendChild(liContact);
 
-        content.appendChild(header);
-    } 
+    navCollapse.appendChild(ul);
+    nav.appendChild(navContainer);
+    header.appendChild(nav);
 
-    return { head }
-}
+    content.appendChild(header);
+  };
 
+  return { head };
+};
 
-export default headerLayout
+export default headerLayout;
--- src/layout/home.js
+++ src/layout/home.js
@@ -1,52 +1,72 @@
-// import { container } from "webpack";
-
+// import { container } from 'webpack';
 
 const home = () => {
-    const sectionTop = () => {
-        const section = document.createElement('section');
-        const itemHome = document.createElement('div');
-        const containerHome = document.createElement('div');
-        const headerContent = document.createElement('div');
-        const headerHome = document.createElement('h1');
-        const paraHome = document.createElement('p');
+  const sectionTop = () => {
+    const section = document.createElement('section');
+    const itemHome = document.createElement('div');
+    const containerHome = document.createElement('div');
+    const headerContent = document.createElement('div');
+    const headerHome = document.createElement('h1');
+    const paraHome = document.createElement('p');
 
-        section.classList.add('section-one');
-        
-        itemHome.classList.add('item');
-        containerHome.classList.add('container');
-        headerContent.classList.add('header-content');
-        headerHome.classList.add('header-title');
+    section.classList.add('section-one');
 
-        headerHome.innerText = 'BEST FOOD';
-        paraHome.classList.add('header-sub-title');
-        paraHome.innerText = 'create your own slogan'
-        // containerHome.appendChild(headerContent);
-        headerContent.appendChild(headerHome)
-        headerContent.appendChild(paraHome);
-        containerHome.appendChild(headerContent);
-        itemHome.appendChild(containerHome)
-        section.appendChild(itemHome)
-        content.appendChild(section);
-    }
+    itemHome.classList.add('item');
+    containerHome.classList.add('container');
+    headerContent.classList.add('header-content');
+    headerHome.classList.add('header-title');
 
-    const sectionSecond = ()=> {
-        const imgClasses = 'mg-responsive section-icon hidden-sm hidden-xs'.split(' ',)
-        const sectionSec = document.createElement('section');
-        const wrapper = document.createElement('div')
-        wrapper.classList.add('wrapper');
-        const containerSecSection = document.createElement('div');
-        containerSecSection.classList.add('container-fluid');
-        wrapper.appendChild(containerSecSection);
-        
-        
-        const imgSection = document.createElement('img');
-        imgSection.classList.add(...imgClasses);
-        sectionSec.appendChild(imgSection)
-        sectionSec.appendChild(wrapper)
-        content.appendChild(sectionSec);
-    }
+    headerHome.innerText = 'BEST FOOD';
+    paraHome.classList.add('header-sub-title');
+    paraHome.innerText = 'create your own slogan';
+    headerContent.appendChild(headerHome);
+    headerContent.appendChild(paraHome);
+    containerHome.appendChild(headerContent);
+    itemHome.appendChild(containerHome);
+    section.appendChild(itemHome);
+    document.getElementById('content').appendChild(section);
+  };
 
-    return {sectionTop, sectionSecond}
-}
+  const sectionSecond = () => {
+    const imgClasses = 'mg-responsive section-icon hidden-sm hidden-xs'.split(' ');
+    const rowTableClasses = 'row dis-table'.split(' ');
+    const hiddenClasses = 'hidden-xs col-sm-6 section-bg about-bg dis-table-cell'.split(' ');
+    const textTableClasses = 'col-xs-12 col-sm-6 dis-table-cell'.split(' ');
+    const sectionSec = document.createElement('section');
+    sectionSec.classList.add('about');
+    sectionSec.setAttribute('id', 'about');
+    const wrapper = document.createElement('div');
+    const rowTable = document.createElement('div');
+    const hidden = document.createElement('div');
+    const textTable = document.createElement('div');
+    const textContainer = document.createElement('div');
+    const headerContainer = document.createElement('h2');
+    headerContainer.classList.add('section-content-title');
+    headerContainer.innerText = 'Awesome Foods';
+    textContainer.classList.add('section-content');
+    textTable.classList.add(...textTableClasses);
+
+    rowTable.classList.add(...rowTableClasses);
+    hidden.classList.add(...hiddenClasses);
+    wrapper.classList.add('wrapper');
+    const containerSecSection = document.createElement('div');
+    containerSecSection.classList.add('container-fluid');
+    textTable.appendChild(textContainer);
+    rowTable.appendChild(hidden);
+    rowTable.appendChild(textTable);
+    textTable.appendChild(textContainer);
+    textContainer.appendChild(headerContainer);
+    containerSecSection.appendChild(rowTable);
+    wrapper.appendChild(containerSecSection);
+
+    const imgSection = document.createElement('img');
+    imgSection.classList.add(...imgClasses);
+    sectionSec.appendChild(imgSection);
+    sectionSec.appendChild(wrapper);
+    document.getElementById('content').appendChild(sectionSec);
+  };
+
+  return { sectionTop, sectionSecond };
+};
 
 export default home;
